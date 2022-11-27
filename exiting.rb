639e35771d25866cf71c6923f95de3d056c8a470
Rayhanuc <rayhanctg4@gmail.com>
2022-11-28 00:17:11 +0600

    4.2 Exit a running script

---
+++ exiting.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+fruits = ['banana', 'apple', 'pear']
+
+fruits.each do |fruit|
+  if fruit == 'apple'
+    # exit
+    # exit!
+    # abort
+    # abort("Exit on apple")
+    redo
+
+  end
+  puts fruit.capitalize
+end
+
+
+puts "Total fruits: #{fruits.count}"
